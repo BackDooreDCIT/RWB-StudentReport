ab2f2dbd8f23625d6ec73943a9bcb4d256607242
nuannim <noempang.parattakorn@gmail.com>
2025-12-26 18:04:06 +0700

    fixed school logo being squished

--- views/_header.php
+++ views/_header.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title><?= isset($title)?htmlspecialchars($title):'App' ?></title>
-  <link rel="stylesheet" href="/static/navbar.css?v=4">
+  <link rel="stylesheet" href="/static/navbar.css?v=5">
   <!-- Minimal shell: no global CSS here to avoid overriding your page styles -->
 </head>
 <body class="has-navbar">
