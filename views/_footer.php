8b94a69d3a7debd0838a564d4ed02443b340c5f5
nuannim <noempang.parattakorn@gmail.com>
2026-01-20 21:54:16 +0700

    score logic invertion

--- views/_footer.php
+++ views/_footer.php
@@ -1,3 +1,14 @@
   </div><!-- /#app -->
+  <script>
+  document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll("form").forEach(function (form) {
+      form.addEventListener("submit", function () {
+        form.querySelectorAll("button, input[type=submit]").forEach(function (btn) {
+          btn.disabled = true;
+        });
+      });
+    });
+  });
+  </script>
 </body>
 </html>
